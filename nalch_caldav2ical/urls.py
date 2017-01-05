7d8999ef5b739d1bdfdba840f5a398ca22433acf
Kristian Scholze <Scholze.Kristian@gmail.com>
2017-01-05 14:46:25 +0100

    add fetching of calendars over basic auth (especially owncloud)

--- nalch_caldav2ical/urls.py
+++ nalch_caldav2ical/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from views import (
     caldav2ical,
+    fetchowncloud,
 )
 
 
 urlpatterns = [
-    url(r'^(?P<calendar>[a-z]+)/$', caldav2ical, name='caldav2ical'),
+    url(r'^fetch/(?P<calendar>[a-z0-9-_]+)/$', fetchowncloud, name='fetchowncloud'),
+    url(r'^(?P<calendar>[a-z0-9-_]+)/$', caldav2ical, name='caldav2ical'),
 ]
--- nalch_caldav2ical/views.py
+++ nalch_caldav2ical/views.py
@@ -1,13 +1,12 @@
 import caldav
+import urllib2
 
 from django.http import (
     HttpResponse,
-    Http404,
 )
 
 from django.shortcuts import (
     get_object_or_404,
-    render_to_response
 )
 
 from nalch_caldav2ical.models import (
@@ -16,10 +15,43 @@ from nalch_caldav2ical.models import (
 
 
 def caldav2ical(request, calendar):
-    calentry = CalendarEntry.objects.get(name=calendar)
-    try:
-        client = caldav.DAVClient(calentry.caldav_url, username=calentry.username, password=calentry.password)
-        calendar = caldav.Calendar(client=client, url=calentry.caldav_url)
-    except NotFoundError:
-        Http404('Calendar not found')
-    return HttpResponse(calendar.events())
+    calentry = get_object_or_404(CalendarEntry, name=calendar)
+    client = caldav.DAVClient(calentry.caldav_url, username=calentry.username, password=calentry.password)
+    calendar = caldav.Calendar(client=client, url=calentry.caldav_url)
+    # todo convert to ical
+    return HttpResponse(calendar)
+
+
+def fetchowncloud(request, calendar):
+    """
+    fetch the ical representation for this calendar from owncloud using Basic Auth and the export
+    :param request: the HTTP Request
+    :type request: HttpRequest
+    :param calendar: The calendar shortname
+    :type calendar: str
+    :return: ical representation of this calendar
+    :rtype: HttpResponse
+    """
+    calentry = get_object_or_404(CalendarEntry, name=calendar)
+    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    # this creates a password manager
+    passman.add_password(None, calentry.caldav_url, calentry.username, calentry.password)
+    # because we have put None at the start it will always
+    # use this username/password combination for  urls
+    # for which `theurl` is a super-url
+
+    authhandler = urllib2.HTTPBasicAuthHandler(passman)
+    # create the AuthHandler
+
+    opener = urllib2.build_opener(authhandler)
+
+    urllib2.install_opener(opener)
+    # All calls to urllib2.urlopen will now use our handler
+    # Make sure not to include the protocol in with the URL, or
+    # HTTPPasswordMgrWithDefaultRealm will be very confused.
+    # You must (of course) use it when fetching the page though.
+
+    response = urllib2.urlopen(calentry.caldav_url + '?export')
+    # authentication is now handled automatically for us
+
+    return HttpResponse(response.read())
